3870650147e7c622356c2df9bb5ab8ee31cd833b
Charlie <cjs0131@gmail.com>
2026-03-22 18:31:59 -0600

    changed fish config to change aliases depending on pkg manager

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,31 +1,45 @@
-\if status is-interactive
-
-
+if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+if command -q pacman
+    alias install='sudo pacman -S'
+    alias update='sudo pacman -Syu'
+    alias remove='sudo pacman -Rs'
+    alias search='pacman -Ss'
+else if command -q zypper
+    alias install='sudo zypper install --no-recommends'
+    alias update='sudo zypper dup'
+    alias remove='sudo zypper remove --clean-deps'
+    alias search='zypper search'
+else if command -q apt
+    alias install='sudo apt install'
+    alias update='sudo apt update && sudo apt upgrade'
+    alias remove='sudo apt remove'
+    alias search='apt search'
+end
 
+# bat — handle Ubuntu naming difference
+if command -q batcat
+    alias bat='batcat'
+end
 
-alias sai='sudo apt install'
-alias sau='sudo apt update'
-alias sauu='sudo apt upgrade'
-alias sar='sudo apt remove'
-alias sas='sudo apt search'
 alias ssh357='ssh cjs@100.72.181.118'
 alias ff='fastfetch'
 alias c='clear'
 alias ..='cd ..'
-alias ....='cd ../..'
-alias fzf='fzf --preview="cat {}"'
-alias bat='batcat'
-alias chezcd="chezmoi cd"
-alias chezedit='chezmoi edit'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias fzf='fzf --preview="bat {}"'
+
+# chezmoi
+alias chcd='chezmoi cd'
 alias chedit='chezmoi edit'
-alias checd='chezmoi cd'
 alias chapply='chezmoi apply'
+
 starship init fish | source
 zoxide init --cmd cd fish | source
+
 # opencode
 fish_add_path /home/charlie/.opencode/bin
-
 eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)
